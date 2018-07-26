de8420579bd5ba221c5f6fc33e86d561b21e3037
Kamil Borowski <kb.kamil.borowski@gmail.com>
2018-07-26 21:43:43 +0200

    .csproj changes

--- src/SSPLibrary.Extensions.Microsoft.EntityFrameworkCore/SSPLibraryQueryableExtensions.cs
+++ src/SSPLibrary.Extensions.Microsoft.EntityFrameworkCore/SSPLibraryQueryableExtensions.cs
@@ -1,8 +1,19 @@
 ﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using SSPLibrary.Models;
 
 namespace Microsoft.EntityFrameworkCore
 {
     public static class SSPLibraryQueryableExtensions
     {
+        public static async Task<PagedResults<T>> ToPagedResultsAsync<T>(
+			this IQueryable<T> source,
+            QueryParameters<T> queryParams,
+            CancellationToken cancellationToken = default(CancellationToken))
+		{
+			throw new NotImplementedException();
+		}
     }
 }
