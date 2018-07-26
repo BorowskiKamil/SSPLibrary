d7b54d5a8e466021fb2fc04929d6dd0eba40874f
Kamil Borowski <kb.kamil.borowski@gmail.com>
2018-07-26 16:55:44 +0200

    Async ToPagedResults

--- src/Extensions/IQueryableExtensions.cs
+++ src/Extensions/IQueryableExtensions.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Linq;
+using System.Threading.Tasks;
 using SSPLibrary.Infrastructure;
 using SSPLibrary.Models;
 
@@ -16,6 +18,27 @@ namespace SSPLibrary
 				.Skip(queryParams.PagingParameters.Offset.Value)
 				.Take(queryParams.PagingParameters.Limit.Value)
 				.ToArray();
+
+			return new PagedResults<T>
+			{
+				TotalSize = totalSize,
+				Items = result
+			};
+		}
+
+		public static async Task<PagedResults<T>> ToPagedResultsAsync<T>(
+			this IQueryable<T> source,
+			Func<IQueryable<T>, Task<int>> countAsync,
+            Func<IQueryable<T>, Task<T[]>> toArrayAsync, 
+            QueryParameters<T> queryParams)
+		{
+			var totalSize = await countAsync(source);
+
+			source = source
+				.Skip(queryParams.PagingParameters.Offset.Value)
+				.Take(queryParams.PagingParameters.Limit.Value);
+			
+			var result = await toArrayAsync(source);
 
 			return new PagedResults<T>
 			{
